f81f97e5313f73cddcb7d5cf5a273e00392aafd6
Даниил Петров <c082kk123@gmail.com>
2024-05-16 22:12:47 +0700

    Добавьте файлы проекта.

---
+++ modelirovanie/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modelirovanie
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConveyorSystem conveyorSystem = new ConveyorSystem(5,0, 4,120); // количество станков, места в буфере, интенсивность прихода заявок, время симуляции прихода заявок в минутах
+
+            
+            conveyorSystem.Simulate();
+        }
+    }
+}
