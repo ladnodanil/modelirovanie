4ce5fcfc0d1551630564d6be3c1a833ee14ec8e4
Даниил Петров <c082kk123@gmail.com>
2024-05-24 10:38:01 +0700

    lg

--- modelirovanie/ConveyorSystem.cs
+++ modelirovanie/ConveyorSystem.cs
@@ -29,12 +29,15 @@ namespace modelirovanie
 
         public Buffer[] Buffers { get; set; } // массив буферов
 
+        public double T_obs { get; set; } // 
+
 
         public Machine(int index, int sizeBuffer)
         {
             Index = index;
             T_osv = 0;
             ServicedDetail = 0;
+            T_obs = 0;
             Buffers = new Buffer[sizeBuffer];
             for (int i = 0; i < Buffers.Length; i++)
             {
@@ -71,16 +74,16 @@ namespace modelirovanie
 
         }
 
-        
 
-        public (double,double,int,int,double) Simulate()
+
+        public (double, double, int, int, double,double) Simulate()
         {
 
-            double t_prich = 0; // время прихода
+            double t_prix = 0; // время прихода
 
             double t_osv = 0; // время освобождения заявки
 
-            int k = 0; // счетчик деталей
+            int N = 0; // счетчик деталей
 
             int N_obs = 0; // счетчик обслуженных деталей
 
@@ -95,21 +98,16 @@ namespace modelirovanie
 
             Random random = new Random();
 
-            while (t_prich < SimulationTime)
+            while (t_prix < SimulationTime)
             {
-                
+
                 bool flag = false;
 
                 bool IsComleted = false;
 
-                t_prich += arrivalTime(random, Lyamda);
-                k++;
-                if (k == 1)
-                {
-                    Console.WriteLine(arrivalTime(random, Lyamda));
-
-                }
-                //Console.WriteLine($"Заявка № {k} пришла в {t_prich*60} секунд");
+                t_prix += arrivalTime(random, Lyamda);
+                N++;
+                //Console.WriteLine($"Заявка № {N} пришла в {t_prix*60} секунд");
 
                 foreach (Machine machine in Machines)
                 {
@@ -117,12 +115,12 @@ namespace modelirovanie
                     {
                         break;
                     }
-                    if (t_prich < machine.T_osv)
+                    if (t_prix < machine.T_osv)
                     {
                         //Console.WriteLine($"Станок № {machine.Index} занят, будет свободен в  {machine.T_osv * 60 + (machine.Index - 1)} секунд");
                         foreach (Buffer buffer in machine.Buffers)
                         {
-                            if (t_prich < buffer.T_osv)
+                            if (t_prix < buffer.T_osv)
                             {
                                 //Console.WriteLine($"Буфер станка № {machine.Index} занят до {buffer.T_osv * 60}");
                                 IsComleted = false;
@@ -132,28 +130,40 @@ namespace modelirovanie
                             {
                                 //Console.WriteLine($"Буфер  станка № {machine.Index} свободен");
                                 double t_nach = machine.T_osv;
-                                //Console.WriteLine($"Время начала обслуживания заявки № {k} - {t_nach * 60} секунд, время в пути до станка {(machine.Index - 1) * 60}");
+                                //Console.WriteLine($"Время начала обслуживания заявки № {N} - {t_nach * 60} секунд, время в пути до станка {(machine.Index - 1) * 60}");
                                 double t_obs = arrivalTime(random, 1);
-
-                                T_obs += t_obs;
-                                //Console.WriteLine($"Время обслуживания заявки № {k} - {t_obs * 60} секунд");
                                 t_osv = t_nach + t_obs;
-                                buffer.T_osv = t_osv;
-                                machine.T_osv = t_osv;
-                                //Console.WriteLine($"Время в буфере:  {(t_nach - t_prich) * 60}");
+                                if (t_osv < SimulationTime)
+                                {
+                                    T_obs += t_obs;
+                                    //Console.WriteLine($"Время обслуживания заявки № {N} - {t_obs * 60} секунд");
+                                    buffer.T_osv = t_osv;
+                                    machine.T_osv = t_osv;
+                                    machine.T_obs += t_obs;
 
-                                T_buffer += (t_nach - t_prich);
+                                    //Console.WriteLine($"Время в буфере:  {(t_nach - t_prix) * 60}");
 
-                                T_preb += (t_obs  + (t_nach - t_prich));
+                                    T_buffer += (t_nach - t_prix);
+
+                                    T_preb += (t_obs + (t_nach - t_prix));
 
 
 
-                                //Console.WriteLine($"Заявка № {k} обслужена в {t_osv * 60} секунд Станком № {machine.Index}\n");
-                                N_obs++;
-                                machine.ServicedDetail++;
-                                flag = true;
-                                IsComleted = true;
-                                break;
+                                    //Console.WriteLine($"Заявка № {N} обслужена в {t_osv * 60} секунд Станком № {machine.Index}\n");
+                                    N_obs++;
+                                    machine.ServicedDetail++;
+                                    flag = true;
+                                    IsComleted = true;
+                                    break;
+
+                                }
+                                else
+                                {
+                                    break;
+                                    continue;
+
+                                }
+
 
                             }
                         }
@@ -161,24 +171,35 @@ namespace modelirovanie
                     else
                     {
                         //Console.WriteLine($"Станок № {machine.Index} свободен");
-                        double t_nach = t_prich + (machine.Index - 1);
-                        //Console.WriteLine($"Время начала обслуживания заявки № {k} - {t_nach * 60} секунд, время в пути до станка {(machine.Index-1) * 60}");
+                        double t_nach = t_prix + (machine.Index - 1);
+                        //Console.WriteLine($"Время начала обслуживания заявки № {N} - {t_nach * 60} секунд, время в пути до станка {(machine.Index-1) * 60}");
                         double t_obs = arrivalTime(random, 1);
-                        T_preb += (t_obs + (machine.Index - 1));
-
-                        T_obs += t_obs;
-                        //Console.WriteLine($"Время обслуживания заявки № {k} - {t_obs * 60} секунд");
                         t_osv = t_nach + t_obs;
-                        machine.T_osv = t_osv;
-                        //Console.WriteLine($"Заявка № {k} обслужена в {t_osv * 60} секунд Станком № {machine.Index}\n");
-                        N_obs++;
-                        machine.ServicedDetail++;
-                        IsComleted = true;
-                        break;
+
+                        //Console.WriteLine($"Время обслуживания заявки № {N} - {t_obs * 60} секунд");
+                        if (t_osv < SimulationTime)
+
+                        {
+                            T_obs += t_obs;
+                            T_preb += (t_obs + (machine.Index - 1));
+                            machine.T_osv = t_osv;
+                            machine.T_obs += t_obs;
+                            //Console.WriteLine($"Заявка № {N} обслужена в {t_osv * 60} секунд Станком № {machine.Index}\n");
+                            N_obs++;
+                            machine.ServicedDetail++;
+                            IsComleted = true;
+                            break;
+
+                        }
+                        else
+                        {
+                            break;
+                            continue;
+                        }
 
                     }
                 }
-                if(IsComleted == false)
+                if (IsComleted == false)
                 {
                     N_neobs++;
                 }
@@ -187,25 +208,32 @@ namespace modelirovanie
                 //Console.WriteLine();
 
             }
-            double ver_obs = (double)N_obs / k ;
-            double ver_neobs = (double)N_neobs / k;
+            double ver_obs = (double)N_obs / N;
+            double ver_neobs = (double)N_neobs / N;
 
             Console.WriteLine("Статистика:");
             Console.WriteLine($"Количество станков: {Machines.Length}");
             Console.WriteLine($"Количество места в буфере: {Machines.Last().Buffers.Length}");
-            Console.WriteLine($"Количество заявок: {k}");
+            Console.WriteLine($"Количество заявок: {N}");
             Console.WriteLine($"Количество обслуженных заявок: {N_obs} - Вероятность обслуживания детали {ver_obs:F2} %");
             Console.WriteLine($"Количество необслуженных заявок: {N_neobs} - Вероятность необслуживания детали {ver_neobs:F2} %");
-
 
             foreach (Machine machine1 in Machines)
             {
                 Console.WriteLine($"Количество обработанных деталей станком № {machine1.Index}: {machine1.ServicedDetail} - Вероятность попадания в станок {(double)machine1.ServicedDetail / N_obs:F2} %");
             }
-            Console.WriteLine($"Cреднее время обслуживания заявок: {T_obs / N_obs * 60} секунд");
-            Console.WriteLine($"Cреднее время в буфере: {T_buffer / N_obs * 60} секунд");
-            Console.WriteLine($"Cреднее время пребывание заявок: {T_preb / N_obs * 60} секунд\n\n");
-            return (ver_obs, T_preb / N_obs * 60,Machines.Length,Machines.Last().Buffers.Length, T_buffer / N_obs * 60);
+            double f = 0;
+            foreach (Machine machine1 in Machines)
+            {
+                //Console.WriteLine($"Время обслуживания деталей станком № {machine1.Index}: {machine1.T_obs / SimulationTime:F2}");
+                f += (machine1.T_obs / SimulationTime);
+
+            }
+            //Console.WriteLine($"{f / Machines.Length:F2}\n\n"); // вероятность загрузки станка
+            //Console.WriteLine($"Cреднее время обслуживания заявок: {T_obs / N_obs * 60} секунд");
+            //Console.WriteLine($"Cреднее время в буфере: {T_buffer / N_obs * 60} секунд");
+            //Console.WriteLine($"Cреднее время пребывание заявок: {T_preb / N_obs * 60} секунд\n\n");
+            return (ver_obs, T_preb / N_obs * 60, Machines.Length, Machines.Last().Buffers.Length, T_buffer / N_obs * 60, f / Machines.Length);
 
         }
 
--- modelirovanie/Program.cs
+++ modelirovanie/Program.cs
@@ -10,27 +10,35 @@ namespace modelirovanie
     {
         static void Main(string[] args)
         {
-            List<(double, double,int,int,double)> stat = new List<(double, double, int, int,double)>();
+            List<(double, double, int, int, double, double)> stat = new List<(double, double, int, int, double, double)>();
 
-            for (int i = 1; i <= 6; i++)
+            for (int i = 1; i <= 49; i++)
             {
-                for (int j = 0; j <= 5; j++)
-                {
+                
 
-                    ConveyorSystem conveyorSystem = new ConveyorSystem(4, 3, 4, 240); // количество станков, места в буфере, интенсивность прихода заявок, время симуляции прихода заявок в минутах
+                    ConveyorSystem conveyorSystem = new ConveyorSystem(3, 5, 4, 240); // количество станков, места в буфере, интенсивность прихода заявок, время симуляции прихода заявок в минутах
                     stat.Add(conveyorSystem.Simulate());
-                }
+                
             }
-            stat.Sort();
-            foreach (var item in stat)
-            {
-                if(item.Item1 >= 0.75)
-                {
-                Console.WriteLine($"Количество станоков {item.Item3}, размер буфера {item.Item4}");
-                Console.WriteLine($"{item.Item1:F2}  {item.Item2:F0}");
 
-                }
-            }
+            //stat.Sort();
+            //double stats = 0;
+            //foreach (var item in stat)
+            //{
+            //    if (item.Item1 >= 0)
+            //    {
+            //        //Console.WriteLine($"Количество станоков {item.Item3}, размер буфера {item.Item4}");
+            //        //Console.WriteLine($"Вероятность обслуживания {item.Item1:F2} , Время пребывания {item.Item2:F0}  , Время ожидание в буфере {item.Item5:F0}");
+            //        //Console.WriteLine($" {item.Item6:F2}");
+            //        //stats += item.Item6;
+            //        //Console.WriteLine($"{item.Item1:F2}  {item.Item2:F0}  {item.Item5:F0}  {item.Item6:F2}");
+            //    }
+            //}
+            Console.WriteLine($"Среднее время ожидания в буфере: {stat.Average(item => item.Item5)}");
+            Console.WriteLine($"Среднее время пребывания: {stat.Average(item => item.Item2)}");
+            Console.WriteLine($"Средняя вероятность обслуживания: {stat.Average(item => item.Item1)}");
+            
+            Console.WriteLine($"Средняя вероятность загрузки станка: {stat.Average(item => item.Item6)}");
 
             Console.ReadLine();
 
